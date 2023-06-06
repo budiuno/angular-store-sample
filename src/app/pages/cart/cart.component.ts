a65725a2a231d482e94e0388a64fa6a643de289e
budiuno <dilapanga1@gmail.com>
2023-06-07 06:41:35 +0700

    done integration to stripe

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -35,7 +37,7 @@ export class CartComponent implements OnInit {
   ]
 
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
@@ -66,7 +68,16 @@ export class CartComponent implements OnInit {
     this.cartService.removeQuantity(item);
   }
 
-
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async (res: any) => {
+      let stripe = await loadStripe('pk_test_51NFo76GWNx5RFk6a0FiYwuMuKZzyfwXC4cGKQTvXQYZ24D1tvofX1r1eKilMQiNXRtynaYlnP5wPatWzqayFW4o500el8q4aDH');
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    })
+  }
   
 
 }
